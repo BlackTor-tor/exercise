832ec7afcd40f3a89024fbd07116fadc42c259e3
BlackTor <603543411@qq.com>
2022-07-17 17:31:27 +0800

    学习main方法

--- TestRun/JavaBasics_High_Test.java
+++ TestRun/JavaBasics_High_Test.java
@@ -7,6 +7,26 @@ package TestRun;
  * @注释 java高级部分测试类
  */
 public class JavaBasics_High_Test {
+    /**关于args的研究，args是专门用来接收命令行参数的。
+     *  如:在一个主类中，运行该程序时： java Test1 365 156 "China"  后面所谓的365 156 "China"就是命令行参数
+     *  在控制台编译时，我在电脑控制台输入了命令：java JavaBasics_High_Test aa bb
+        根据下面的代码，输出结果为：
+        0   aa
+        1   bb
+
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println( i + "\t" + args[i]);
+        }*/
+//        JavaBasics_High_Test.main(new String[]{"11", "22"});//会报errorOutstanding错误，栈溢出
+    public static void main(String[] args) {
+
+
+
+
+    }
+
 
 
 }
+
